5b460999a3064f106df7c143a214cbc90609a59f
Eric Zhang <ericspring08@gmail.com>
2022-10-13 21:08:10 -0500

    add dividers and add title to results page

--- pages/results.tsx
+++ pages/results.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";
+import Head from "next/head";
 import { useEffect, useState } from "react";
 
 const Results: NextPage = () => {
@@ -15,19 +16,28 @@ const Results: NextPage = () => {
     }, [])
 
     return (
-        <main className="flex flex-auto flex-col bg-black p-20">
-            <h1 className="text-white text-center text-8xl font-mono">Results</h1>
-            {
-                animals.map((animal, index) => {
-                    return (
-                        <div key={index} className="flex flex-row justify-between">
-                            <h1 className="text-white text-center text-4xl font-mono justify-self-start">{index + 1}. {animal.animal}</h1>
-                            <h1 className="text-white text-center text-4xl font-mono">{animal.wins} wins, {animal.loses} loses</h1>
-                        </div>
-                    )
-                })
-            }
-        </main>
+        <div>
+            <Head>
+                <title>Results</title>
+                <meta name="description" content="Find out who is the strongest animal?" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main className="flex flex-auto flex-col bg-black p-20">
+                <h1 className="text-white text-center text-8xl font-mono">Results</h1>
+                <div className="divide-y">
+                    {
+                        animals.map((animal, index) => {
+                            return (
+                                <div key={index} className="flex flex-row justify-between p-2">
+                                    <h1 className="text-white text-center text-4xl font-mono justify-self-start">{index + 1}. {animal.animal}</h1>
+                                    <h1 className="text-white text-center text-4xl font-mono">{animal.wins} wins, {animal.loses} loses</h1>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </main>
+        </div>
     )
 }
 
